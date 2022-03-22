b9c1ea9f525ecefd993f515656d89a9af0509df7
Nguyễn Quang Duy <nqduy98999@gmail.com>
2022-03-22 19:38:44 +0700

    Finished building Chat App using ReactJS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { ChatEngine } from 'react-chat-engine'
 import './App.css'
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  if(!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <ChatEngine
       height="100vh"
       projectID="
       ae8408bc-bbf7-4410-b3a8-21c2c5cecde4"
-      userName="javascriptmastery"
-      userSecret="123456"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 
     />
   );
